2fc8a6e0ae9e8d8a2d0e1cf8e43fcf33cbd8d7f4
Owais <oowais@gmail.com>
2024-01-13 02:27:03 +0100

    🚅 perfs: color-mode module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [["unplugin-icons/nuxt", { autoInstall: true }], "@nuxt/ui"],
+  modules: [
+    ["unplugin-icons/nuxt", { autoInstall: true }],
+    "@nuxt/ui",
+    "@nuxtjs/color-mode",
+  ],
 });
